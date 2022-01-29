1597b39de4c8c840d6f320a0128a6f9be21722aa
CyberSpaceBoy <80852240+CyberSpaceBoy@users.noreply.github.com>
2022-01-29 10:28:34 -0800

    Create Shell.cs

---
+++ Funnix/funnix/os/core/Shell.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace openFunnix.os
+{
+    class Shell
+    {
+        public static void main()
+        {
+            Console.WriteLine("Booted into Shell");
+   
+            Console.WriteLine(openFunnix.os.core.shell.ArtClass.friend);
+
+            Console.WriteLine("Nothing here yet. Stay tuned!");
+            Console.WriteLine("Version: openFunnix");
+
+
+        }
+    }
+}
