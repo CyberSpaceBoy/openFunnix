3955c4f654b45baf7f9ae5204a0de00d6f28864a
CyberSpaceBoy <80852240+CyberSpaceBoy@users.noreply.github.com>
2022-01-29 10:27:20 -0800

    Update Kernel.cs

--- Funnix/funnix/Kernel.cs
+++ Funnix/funnix/Kernel.cs
@@ -1,1 +1,19 @@
 
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Sys = Cosmos.System;
+namespace Kernel { 
+
+
+    public class Kernel : Sys.Kernel
+    {
+        protected override void BeforeRun() {
+            Console.Clear();
+           Console.WriteLine("Loaded into Boot");
+            openFunnix.os.Shell.main();
+        }
+
+        protected override void Run(){}
+    }
+}
